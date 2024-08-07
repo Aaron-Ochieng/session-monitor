440bc7f1ad8e8bccceb5963606e00adf3dd83276
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-07 15:45:11 +0300

    Add(unix logs): Checking unix logs This is achieved by executing the unix last command to list all the data in wtmp log file

--- utils/unix_logs.go
+++ utils/unix_logs.go
@@ -1,5 +1,11 @@
 package session
 
+import (
+	"os/exec"
+	"strings"
+	"time"
+)
+
 func trimspace(s string) (res []string) {
 	temp := ""
 	for _, char := range s {
@@ -14,4 +20,39 @@ func trimspace(s string) (res []string) {
 		res = append(res, temp)
 	}
 	return res
+}
+
+func UnixLog() (logininfo []LoginInfo, err error) {
+	command, err := exec.Command("last", "-f", "/var/log/wtmp").Output()
+	if err != nil {
+		return nil, err
+	}
+	mac_address, err := GetMacAddress()
+	if err != nil {
+		return nil, err
+	}
+	res := strings.Split(string(command), "\n")
+	// Remove the 2 extra space and wtmp information of startuptime
+	res = res[:len(res)-3]
+
+	for _, val := range res {
+		info := trimspace(val)
+		if info[0] == "bocal" {
+			continue
+		}
+		temp := LoginInfo{}
+		temp.Username = info[0]
+		temp.Date.Day = info[5]
+		temp.Date.Month = info[4]
+		temp.Date.Year = time.Now().Year()
+		temp.LoginTime = info[6]
+		temp.LogoutTime = info[8]
+		if info[8] == "logged" {
+			temp.DeviceId = mac_address
+		}
+		if info[0] != "reboot" {
+			logininfo = append(logininfo, temp)
+		}
+	}
+	return logininfo, nil
 }
