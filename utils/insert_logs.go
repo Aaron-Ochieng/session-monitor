5b19af4ce4bd5029372606cad715f1f12626ffe9
Aaron Ochieng <ochiengaoron@outlook.com>
2024-08-11 16:11:53 +0300

    Add: Create tables functionality if the do not exist.

--- utils/insert_logs.go
+++ utils/insert_logs.go
@@ -21,3 +21,38 @@ func connection() (db *sql.DB, err error) {
 	return db, nil
 }
 
+func InitDbTables() error {
+	db, err := connection()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	_, err = db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS users(
+			id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+			username VARCHAR(20) NOT NULL UNIQUE
+			
+		);
+		CREATE TABLE IF NOT EXISTS userStats(
+      id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+			macAddress VARCHAR(20) NOT NULL,
+			userId INTEGER NOT NULL,
+			date VARCHAR(11),
+			loginTime  VARCHAR(12) NOT NULL,
+			logoutTime VARCHAR(12),
+			FOREIGN KEY (userId) REFERENCES users(id)
+		);
+
+    CREATE TABLE IF NOT EXISTS lastInsertDate(
+      macAddress VARCHAR(20) NOT NULL PRIMARY KEY,
+      date VARCHAR(11)
+    );      
+		`,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
