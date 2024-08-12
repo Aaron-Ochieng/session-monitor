a983e77142674cca8d71a275c4beed2703fb6cef
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-12 08:24:33 +0300

    Add functions to manage state file for program run tracking
    - Implement `fileExists` function to check if the state file exists.
    - Implement `createStateFile` function to create a state file indicating the program has run.
    - These functions are used to track whether the program is running for the first time or not.

---
+++ utils/check_file.go
@@ -0,0 +1,22 @@
+package session
+
+import (
+	"log"
+	"os"
+)
+
+const stateFilePath = "/var/tmp/session-monitor"
+
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func CreateStateFile(path string) {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("Error creating state file: %v\n", err)
+		return
+	}
+	defer file.Close()
+}
