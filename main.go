fde15592b090d3587127d2fbb5759a45024a9bd4
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-12 12:45:19 +0300

    Add: programs' entry point

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	session "session/utils"
+)
 
 func main() {
-	fmt.Println("session monitor")
+	// check if its first run
+	is_first_run := !session.FileExists(session.TempFilePath)
+	if is_first_run {
+		session.InitDbTables()
+		session.CreateStateFile(session.TempFilePath)
+	}
+	logs, _ := session.UnixLog()
+	session.InsertUsername(logs)
+	session.InsertLog(is_first_run)
+	fmt.Println(session.BatteryStatus())
 }
