b17b159b50446c12cf9b8c59098046392a205a0e
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-12 13:12:57 +0300

    feat: add User and UserLog structs for ORM table generation
    - Define `User` struct to represent users table with `ID` and `Username` fields.
    - Define `UserLog` struct to represent user logs with fields for `ID`, `MacAddress`, `UserId`, `Date`, `LoginTime`, `LogoutTime`, and `Uptime`.
    - Establish a relationship between `UserLog` and `User` using `pg` ORM tags.
    - Ensure fields are appropriately tagged for automatic table creation with go-pg.

--- utils/login_info_struct.go
+++ utils/login_info_struct.go
@@ -1,5 +1,22 @@
 package session
 
+type User struct {
+	ID       int64  `pg:"id,pk,autoincrement"`
+	Username string `pg:"username,unique,notnull"`
+}
+
+// UserLog represents the userlogs table.
+type UserLog struct {
+	ID         int64   `pg:"id,pk,autoincrement"`
+	MacAddress string  `pg:"macAddress,notnull"`
+	UserId     int64   `pg:"userId,notnull"`
+	Date       string  `pg:"date"`
+	LoginTime  string  `pg:"loginTime,notnull"`
+	LogoutTime string  `pg:"logoutTime"`
+	Uptime     float64 `pg:"hours"`
+	User       *User   `pg:"rel:has-one,join_fk:userId"`
+}
+
 type LoginInfo struct {
 	Username   string
 	Date       string
